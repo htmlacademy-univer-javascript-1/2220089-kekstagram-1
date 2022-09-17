73c4982862622371c373e70deb80bb220256e771
Keks <keks@htmlacademy.ru>
2022-09-17 13:02:44 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,20 @@
+const getRandomInteger = (min, max) => {
+  if (min < 0 || max < 0 || max <= min) {
+    return -1;
+  }
+  else {
+    const rand = Math.ceil(min) + Math.floor(Math.random() * (Math.floor(max) + 1 - Math.ceil(min)));
+    return rand;
+  }
+};
+getRandomInteger(-0.35, 2.38);
+
+const getLineLength = (line, maxLength) => {
+  if (line.length >= maxLength) {
+    return false;
+  }
+  else {
+    return true;
+  }
+};
+getLineLength('у э', 5);
