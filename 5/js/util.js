2e0aa3d6b1fa3d12e7673c23fcf6912a59690022
Keks <keks@htmlacademy.ru>
2022-11-22 14:36:22 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/util.js
@@ -0,0 +1,13 @@
+const getRandomInteger = (min, max) => {
+  if (min < 0 || max < 0 || min === max) {
+    return -1;
+  }
+  if (max < min){
+    [max, min] = [min, max];
+  }
+  return Math.ceil(min) + Math.floor(Math.random() * (Math.floor(max) + 1 - Math.ceil(min)));
+};
+
+const getLineLength = (line, maxLength) => line.length <= maxLength;
+
+export{getRandomInteger, getLineLength};
