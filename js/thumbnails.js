2c7c50c8021a8aa9a1d2c389b3bb620152cfde3e
grittyyy <grittyyy@outlook.com>
2022-12-07 23:03:45 +0500

    Починил Escape

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -4,13 +4,13 @@ const template = document.querySelector('#picture').content;
 const documentFragment = document.createDocumentFragment();
 const pictures = document.querySelector('.pictures');
 
-const createThumbnails = (data, index) => {
+const createThumbnails = (data) => {
   data.forEach((image) => {
     const picture = template.cloneNode(true);
     picture.querySelector('.picture__img').src = image.url;
     picture.querySelector('.picture__likes').textContent = image.likes;
     picture.querySelector('.picture__comments').textContent = image.comments.length;
-    picture.querySelector('.picture').dataset.index = index;
+    picture.querySelector('.picture').dataset.index = image.id;
     documentFragment.append(picture);
   });
   pictures.append(documentFragment);
